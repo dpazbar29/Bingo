34e2464d7fa32464b17c5d9ffee902bf8b316f9b
dpazbar29 <145806563+dpazbar29@users.noreply.github.com>
2024-04-29 08:42:52 +0200

    Clikt añadido

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:3.14.9")
+    runtimeOnly("com.github.ajalt.clikt:clikt:4.4.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
 }
